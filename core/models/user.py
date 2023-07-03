47ca43931950d271543b95b94b86c2899d21a08c
Mahdi Farhang <mafarhang76@gmail.com>
2023-07-03 20:48:52 +0330

    Change UserModel shallow delete to use global way

--- core/models/user.py
+++ core/models/user.py
@@ -1,55 +1,36 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.utils import timezone
+from django.contrib.auth.models import UserManager as DefaultUserManager
+
+from core.models.mixins import (
+	ShallowDeleteModel,
+	ShallowDeleteModelManager,
+	ShallowDeleteModelAllManager,
+)
 
 
-class UserQueryset(models.QuerySet):
-	def delete(self):
-		return self.update(is_deleted=True)
+class UserAllManager(ShallowDeleteModelAllManager, DefaultUserManager):
+	pass
 
 
-class UserManager(models.Manager):
-	def get_queryset(self):
-		return UserQueryset(
-			model=self.model,
-			using=self._db
-		).filter(is_deleted=False)
+class UserManager(ShallowDeleteModelManager, DefaultUserManager):
+	pass
 
 
-class User(AbstractUser):
-	"""
-	UserModel supports shallow delete. meaning we usually don't delete objects on this model.
-	Instead, we set is_deleted field to True.
-	In order not to have errors, objects is changed
-	"""
-	is_deleted = models.BooleanField(default=False, null=False)
-	deleted_at = models.DateTimeField(null=True, blank=True, default=None)
+class User(AbstractUser, ShallowDeleteModel):
 
 	objects = UserManager()
-	all_objects = models.Manager()
-
-	def delete(self, using=None, keep_parents=False):
-		# TODO: Handle objects referring to this user and have on_delete=CASCADE
-		self.is_deleted = True
-		self.deleted_at = timezone.now()
-		self.save()
-
-	def restore(self):
-		if not self.is_deleted:
-			return
-		self.is_deleted = False
-		self.deleted_at = None
-		self.save()
-
+	all_objects = UserAllManager()
 
 	def represent(self):
+		result = 'User '
 		if self.last_name:
 			if self.first_name:
-				return f'{self.first_name} {self.last_name}'
-			return self.last_name
+				return result + f'{self.first_name} {self.last_name}'
+			return result + self.last_name
 		if self.email:
-			return self.email
-		return self.username
+			return result + self.email
+		return result + self.username
 
 	def __str__(self):
 		return self.represent()
