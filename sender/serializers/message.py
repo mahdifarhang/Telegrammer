3d72923f1b49a903693ea2161110b84ae6c96094
Mahdi Farhang <mafarhang76@gmail.com>
2023-12-04 04:47:42 +0330

    Serialization structure for Message

--- sender/serializers/message.py
+++ sender/serializers/message.py
@@ -1,5 +1,29 @@
 from rest_framework import serializers
 
+from sender.models import Message
 
 class MessageSerializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = Message
+        fields = [
+            'receiver_id',
+            'chat_id',
+            'project_id',
+            'sender_bot_id',
+            'text',
+        ]
+        # TODO: Test if fields are given okay. definitely need more validations,
+        # meaning we have to check if the _id is correct, and check for read_only, write_only attributes.
+
+
+    def save(self, **kwargs):
+        pass
+        # TODO: Implement saving Message object here, getting data from kwargs
+
+    def validate_project_id(self, data):
+        pass
+        # TODO: Implement
+
+    def validate_sender_bot_id(self, data):
+        # TODO: Implement
+        pass
--- sender/views.py
+++ sender/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 
 from core.permissions import IsActiveAuthenticated
 from sender.serializers import MessageSerializer
+from sender.models import Message
 
 
 class SendMessage(GenericAPIView):
@@ -17,5 +18,13 @@ class SendMessage(GenericAPIView):
         return Response(data=response, status=status.HTTP_200_OK)
 
     def send_message(serializer):
-        pass
+        extra_content = {
+            "status": Message.StatusChoices.SENDING,
+        }
+        serializer.save(extra_content)
+        # TODO: Start sending telegram message task
 
+class CheckSentMessage():
+    pass
+    # TODO Implement
+    # Important: Two things could happen, return the status of the message object, or check if the task for sending this message is being done or not, and check if the task is okay!
